82052ebee6766e9ac157bfc2474b77678222c3d0
anthonyamar <anthonyamar@labodessons.fr>
2019-08-20 23:00:24 +0200

    create number_to_currency on show, transform in responsive website

--- app/helpers/real_estates_helper.rb
+++ app/helpers/real_estates_helper.rb
@@ -1,2 +1,11 @@
 module RealEstatesHelper
+  include ActionView::Helpers::NumberHelper
+  
+  def to_percentage(number)
+    number_to_currency(number, unit: "%", separator: ",", delimiter: "", format: "%n %u")
+  end
+  
+  def to_currency(number)
+    number_to_currency(number, unit: "€", separator: ",", delimiter: "", format: "%n %u")
+  end
 end
